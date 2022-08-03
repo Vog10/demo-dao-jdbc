b09044c0adaf43ae885ef60c4ea4e6658f9b6086
vog10 <vog10@github.com>
2022-08-02 22:45:38 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,7 +12,10 @@ public class Program {
 		
 		Department obj = new Department(1, "Books");
 		
+		Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(),3000.0, obj);
+		
 		System.out.println(obj);
+		System.out.println(seller);
 
 	}
 
